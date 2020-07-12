fcd53f61c4fc7a91ee420cf47d70f87ab5333504
Dirk <you@example.com>
2020-07-12 18:30:22 +0200

    Adds movie catalog API

---
+++ movie-catalog-service/src/main/java/codes/drkfrtz/moviecatalogservice/model/CatalogItem.java
@@ -0,0 +1,26 @@
+package codes.drkfrtz.moviecatalogservice.model;
+
+public class CatalogItem {
+
+    private String title;
+    private String description;
+    private int rating;
+
+    public CatalogItem(String title, String description, int rating) {
+        this.title = title;
+        this.description = description;
+        this.rating = rating;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+}
---
+++ movie-catalog-service/src/main/java/codes/drkfrtz/moviecatalogservice/resources/MovieCatalogResource.java
@@ -0,0 +1,22 @@
+package codes.drkfrtz.moviecatalogservice.resources;
+
+import codes.drkfrtz.moviecatalogservice.model.CatalogItem;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.annotation.RequestScope;
+
+import java.util.Collections;
+import java.util.List;
+
+@RestController
+@RequestMapping("/catalog")
+public class MovieCatalogResource {
+
+    @RequestMapping("/{userId}")
+    public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
+        return Collections.singletonList(
+                new CatalogItem("Iron Man", "Coolest super hero in DC universe.", 5)
+        );
+    }
+}
