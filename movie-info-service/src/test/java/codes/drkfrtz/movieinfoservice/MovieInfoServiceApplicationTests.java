ebbfbbe182a68be7bf672ed6f1b547561bf00d37
Dirk <you@example.com>
2020-07-12 18:29:36 +0200

    Sets up 3 springboot applications

---
+++ movie-info-service/src/test/java/codes/drkfrtz/movieinfoservice/MovieInfoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package codes.drkfrtz.movieinfoservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieInfoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ ratings-data-service/src/main/java/codes/drkfrtz/ratingsdataservice/RatingsDataServiceApplication.java
@@ -0,0 +1,13 @@
+package codes.drkfrtz.ratingsdataservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RatingsDataServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RatingsDataServiceApplication.class, args);
+	}
+
+}
